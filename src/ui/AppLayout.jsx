4489dad172875aedeccbb5e2866ab47029302cbc
n-korel <n.korelskiy@hotmail.com>
2025-03-12 18:34:33 +0300

    Handling Form Errors

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,6 +13,18 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
@@ -21,7 +33,9 @@ function AppLayout() {
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
